f22c95360c25a22e27894cf2f959e113ccb938af
Lan <lan.peng234@gmail.com>
2023-07-27 12:38:32 -0700

    Add builder pattern in User

--- src/main/java/com/laioffer/staybooking/model/User.java
+++ src/main/java/com/laioffer/staybooking/model/User.java
@@ -1,16 +1,28 @@
 package com.laioffer.staybooking.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "user")
+@JsonDeserialize(builder = User.Builder.class)
 public class User {
     @Id
     private String username;
     private String password;
     private boolean enabled;
+
+    public User() {}
+
+    private User(Builder builder) {
+        this.username = builder.username;
+        this.password = builder.password;
+        this.enabled = builder.enabled;
+    }
 
     public String getUsername() {
         return username;
@@ -37,5 +49,35 @@ public class User {
     public User setEnabled(boolean enabled) {
         this.enabled = enabled;
         return this;
+    }
+
+    public static class Builder {
+        @JsonProperty("username")
+        private String username;
+
+        @JsonProperty("password")
+        private String password;
+
+        @JsonProperty("enabled")
+        private boolean enabled;
+
+        public Builder setUsername(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder setEnabled(boolean enabled) {
+            this.enabled = enabled;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
     }
 }
