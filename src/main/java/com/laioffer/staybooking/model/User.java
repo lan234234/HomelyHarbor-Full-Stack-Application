f79f84cab69052d52d1f91b1e7146e6b8c173e72
Lan <lan.peng234@gmail.com>
2023-07-26 23:31:24 -0700

    Add public Getter and Setter for each private field

--- src/main/java/com/laioffer/staybooking/model/User.java
+++ src/main/java/com/laioffer/staybooking/model/User.java
@@ -11,4 +11,31 @@ public class User {
     private String username;
     private String password;
     private boolean enabled;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public User setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public User setEnabled(boolean enabled) {
+        this.enabled = enabled;
+        return this;
+    }
 }
