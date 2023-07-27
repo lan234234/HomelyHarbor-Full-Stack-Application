57e75c014be9b92c9c0853ea490f704dae5fc7e5
Lan <lan.peng234@gmail.com>
2023-07-26 23:08:22 -0700

    Add Hibernate-related annotation in User class

--- src/main/java/com/laioffer/staybooking/model/User.java
+++ src/main/java/com/laioffer/staybooking/model/User.java
@@ -1,6 +1,13 @@
 package com.laioffer.staybooking.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
     private String username;
     private String password;
     private boolean enabled;
