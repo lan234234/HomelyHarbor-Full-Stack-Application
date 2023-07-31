4bd65459524c060dab65986530e1a230325f511b
Lan <lan.peng234@gmail.com>
2023-07-31 14:48:30 -0700

    Update SecurityConfig to allow /authentication path

--- src/main/java/com/laioffer/staybooking/config/SecurityConfig.java
+++ src/main/java/com/laioffer/staybooking/config/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
 import javax.sql.DataSource;
 
 @EnableWebSecurity
@@ -32,6 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/register/*").permitAll()
+                .antMatchers(HttpMethod.POST, "/authenticate/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .csrf()
