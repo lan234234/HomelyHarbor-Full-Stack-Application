e8e3a9fe6e4a14825517f8e171cbe02009fc0aaf
Lan <lan.peng234@gmail.com>
2023-07-31 14:20:32 -0700

    Add generateToken() in JwtUtil

--- src/main/java/com/laioffer/staybooking/util/JwtUtil.java
+++ src/main/java/com/laioffer/staybooking/util/JwtUtil.java
@@ -1,4 +1,27 @@
 package com.laioffer.staybooking.util;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+
+@Component
 public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String generateToken(String subject) {
+        return Jwts.builder()
+                .setClaims(new HashMap<>())
+                .setSubject(subject)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 864000000))
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
 }
