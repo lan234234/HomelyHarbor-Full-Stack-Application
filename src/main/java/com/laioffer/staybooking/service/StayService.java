15996a7929c20de35aa76d92dc8e53b46a4d2835
Lan <lan.peng234@gmail.com>
2023-08-02 17:04:00 -0700

    Update add() to support image saving in StayService

--- src/main/java/com/laioffer/staybooking/service/StayService.java
+++ src/main/java/com/laioffer/staybooking/service/StayService.java
@@ -2,18 +2,27 @@ package com.laioffer.staybooking.service;
 
 import com.laioffer.staybooking.exception.StayNotExistException;
 import com.laioffer.staybooking.model.Stay;
+import com.laioffer.staybooking.model.StayImage;
 import com.laioffer.staybooking.model.User;
 import com.laioffer.staybooking.repository.StayRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 public class StayService {
 
+    private final ImageStorageService imageStorageService;
     private final StayRepository stayRepository;
 
-    public StayService(StayRepository stayRepository) {
+
+    public StayService(ImageStorageService imageStorageService, StayRepository stayRepository) {
+        this.imageStorageService = imageStorageService;
         this.stayRepository = stayRepository;
     }
 
@@ -29,7 +38,15 @@ public class StayService {
         return stay;
     }
 
-    public void add(Stay stay) {
+
+    public void add(Stay stay, MultipartFile[] images) {
+        List<StayImage> stayImages = Arrays.stream(images)
+                .filter(image -> !image.isEmpty())
+                .parallel()
+                .map(imageStorageService::save)
+                .map(mediaLink -> new StayImage(mediaLink, stay))
+                .collect(Collectors.toList());
+        stay.setImages(stayImages);
         stayRepository.save(stay);
     }
 
