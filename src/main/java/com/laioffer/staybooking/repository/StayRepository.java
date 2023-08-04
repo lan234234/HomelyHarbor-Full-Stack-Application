04c233ac382f116cc67369fc518be3aee07104f9
Lan <lan.peng234@gmail.com>
2023-08-04 14:09:44 -0700

    Add findByIdInAndGuestNumberGreaterThanEqual() in StayRepository

--- src/main/java/com/laioffer/staybooking/repository/StayRepository.java
+++ src/main/java/com/laioffer/staybooking/repository/StayRepository.java
@@ -13,4 +13,7 @@ public interface StayRepository extends JpaRepository<Stay, Long> {
     List<Stay> findByHost(User user);
 
     Stay findByIdAndHost(Long id, User host);
+
+    List<Stay> findByIdInAndGuestNumberGreaterThanEqual(List<Long> ids, int guestNumber);
+
 }
