6c1a0b6e3605ca89f44fb524fba02e91f42f56db
Lan <lan.peng234@gmail.com>
2023-08-06 10:33:56 -0700

    Update StayController to read user data from token

--- src/main/java/com/laioffer/staybooking/controller/StayController.java
+++ src/main/java/com/laioffer/staybooking/controller/StayController.java
@@ -6,6 +6,7 @@ import com.laioffer.staybooking.service.StayService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -18,15 +19,14 @@ public class StayController {
     }
 
     @GetMapping(value = "/stays")
-    public List<Stay> listStays(@RequestParam(name = "host") String hostName) {
-        return stayService.listByUser(hostName);
+    public List<Stay> listStays(Principal principal) {
+        return stayService.listByUser(principal.getName());
     }
 
-    @GetMapping(value = "/stays/id")
-    public Stay getStay(
-            @RequestParam(name = "stay_id") Long stayId,
-            @RequestParam(name = "host") String hostName) {
-        return stayService.findByIdAndHost(stayId, hostName);
+
+    @GetMapping(value = "/stays/{stayId}")
+    public Stay getStay(@PathVariable Long stayId, Principal principal) {
+        return stayService.findByIdAndHost(stayId, principal.getName());
     }
 
     @PostMapping("/stays")
@@ -34,25 +34,24 @@ public class StayController {
             @RequestParam("name") String name,
             @RequestParam("address") String address,
             @RequestParam("description") String description,
-            @RequestParam("host") String host,
             @RequestParam("guest_number") int guestNumber,
-            @RequestParam("images") MultipartFile[] images
-    ) {
+            @RequestParam("images") MultipartFile[] images,
+            Principal principal) {
+
+
         Stay stay = new Stay.Builder()
                 .setName(name)
                 .setAddress(address)
                 .setDescription(description)
                 .setGuestNumber(guestNumber)
-                .setHost(new User.Builder().setUsername(host).build())
+                .setHost(new User.Builder().setUsername(principal.getName()).build())
                 .build();
         stayService.add(stay, images);
     }
 
-    @DeleteMapping("/stays")
-    public void deleteStay(
-            @RequestParam(name = "stay_id") Long stayId,
-            @RequestParam(name = "host") String hostName) {
-        stayService.delete(stayId, hostName);
-    }
 
+    @DeleteMapping("/stays/{stayId}")
+    public void deleteStay(@PathVariable Long stayId, Principal principal) {
+        stayService.delete(stayId, principal.getName());
+    }
 }
