68f249e0c17fd7193b42ff05154c8e99c084235b
Lan <lan.peng234@gmail.com>
2023-08-06 17:01:55 -0700

    Update StayService to save location information in Elasticsearch

--- src/main/java/com/laioffer/staybooking/service/StayService.java
+++ src/main/java/com/laioffer/staybooking/service/StayService.java
@@ -1,14 +1,17 @@
 package com.laioffer.staybooking.service;
 
 import com.laioffer.staybooking.exception.StayNotExistException;
+import com.laioffer.staybooking.model.Location;
 import com.laioffer.staybooking.model.Stay;
 import com.laioffer.staybooking.model.StayImage;
 import com.laioffer.staybooking.model.User;
+import com.laioffer.staybooking.repository.LocationRepository;
 import com.laioffer.staybooking.repository.StayRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
+
+import javax.transaction.Transactional;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,11 +22,15 @@ public class StayService {
 
     private final ImageStorageService imageStorageService;
     private final StayRepository stayRepository;
+    private final GeoCodingService geoCodingService;
+    private final LocationRepository locationRepository;
 
 
-    public StayService(ImageStorageService imageStorageService, StayRepository stayRepository) {
+    public StayService(ImageStorageService imageStorageService, StayRepository stayRepository, GeoCodingService geoCodingService, LocationRepository locationRepository) {
         this.imageStorageService = imageStorageService;
         this.stayRepository = stayRepository;
+        this.geoCodingService = geoCodingService;
+        this.locationRepository = locationRepository;
     }
 
     public List<Stay> listByUser(String username) {
@@ -39,6 +46,7 @@ public class StayService {
     }
 
 
+    @Transactional
     public void add(Stay stay, MultipartFile[] images) {
         List<StayImage> stayImages = Arrays.stream(images)
                 .filter(image -> !image.isEmpty())
@@ -48,6 +56,9 @@ public class StayService {
                 .collect(Collectors.toList());
         stay.setImages(stayImages);
         stayRepository.save(stay);
+
+        Location location = geoCodingService.getLatLng(stay.getId(), stay.getAddress());
+        locationRepository.save(location);
     }
 
     public void delete(Long stayId, String username) throws StayNotExistException {
