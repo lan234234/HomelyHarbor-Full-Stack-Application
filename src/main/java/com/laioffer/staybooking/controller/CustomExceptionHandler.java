0b347eea9a369ed0025f6340d50d9227d065afb3
Lan <lan.peng234@gmail.com>
2023-08-06 17:03:41 -0700

    Add two handlers for GeoCoding errors in CustomExceptionHandler

--- src/main/java/com/laioffer/staybooking/controller/CustomExceptionHandler.java
+++ src/main/java/com/laioffer/staybooking/controller/CustomExceptionHandler.java
@@ -35,4 +35,14 @@ public class CustomExceptionHandler {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(GeoCodingException.class)
+    public final ResponseEntity<String> handleGeoCodingExceptions(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    @ExceptionHandler(InvalidStayAddressException.class)
+    public final ResponseEntity<String> handleInvalidStayAddressExceptions(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
